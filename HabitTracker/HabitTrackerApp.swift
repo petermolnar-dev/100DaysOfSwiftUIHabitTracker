249a1de83dde4abf1d4a36c9badb794783e363c7
Peter Molnar <petermolnar.hu@gmail.com>
2022-01-06 19:41:44 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Peter Molnar on 06/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
