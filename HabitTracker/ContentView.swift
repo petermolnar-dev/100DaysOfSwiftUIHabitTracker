1f9d0775a78d9386d609a1791cbda6e6bac48f70
Peter Molnar <petermolnar@financestack.com>
2022-01-11 13:45:58 +0100

    List layout and toolbar button. Adding random activities.

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -9,9 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @StateObject var activityStore = ActivityStore()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List{
+                ForEach(activityStore.activities) { activity in
+                    HStack {
+                        Text(activity.name)
+                            .font(.headline)
+                        Spacer()
+                        Text(activity.count, format:  .number)
+                            .fontWeight(.light)
+                    }
+                }
+            }
+            .navigationTitle("Activities")
+            .toolbar {
+                Button {
+                    addNewActivity()
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+    }
+    
+    func addNewActivity() {
+        let activity = Activity(id: UUID(),
+                                name: "Whatever", count: Int.random(in: 1...300))
+        activityStore.activities.append(activity)
     }
 }
 
